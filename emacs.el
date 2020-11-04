9bea20015ecbb8ee43b66e866a69b4c7fb8173a1
Tristan Cacqueray <tdecacqu@redhat.com>
2020-11-04 01:30:40 +0000

    emacs: add show-file-name function

--- emacs.el
+++ emacs.el
@@ -139,5 +139,10 @@
       (git-clone-url url d))
     (dired d)))
 
+(defun show-file-name ()
+  "Show the full path file name in the minibuffer."
+  (interactive)
+  (message (buffer-file-name)))
+
 (provide 'init)
 ;;; emacs ends here
