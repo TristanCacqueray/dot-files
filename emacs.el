523a670c384eed16c24aa24dfe61af02f3884bd0
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-03 13:39:20 +0000

    emacs: activate winner-mode

--- emacs.el
+++ emacs.el
@@ -199,5 +199,7 @@
                                  frame-name
                                  "\" -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS '0x2, 0x0, 0x0, 0x0, 0x0'"))))
 
+(winner-mode)
+
 (provide 'init)
 ;;; emacs ends here
