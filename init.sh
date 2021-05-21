faffaa5e8d4699829a5162d2c5b5f29dc6c15ef4
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-21 22:41:04 +0000

    init: add boot script

---
+++ init.sh
@@ -0,0 +1,78 @@
+#!/bin/sh -e
+# A script to setup a fresh system
+
+# Copy paste:
+cat <<EOF>/dev/null
+mkdir -p ~/src/github.com/TristanCacqueray/dot-files
+git clone https://github.com/TristanCacqueray/dot-files ~/src/github.com/TristanCacqueray/dot-files
+ln -sf $(realpath ~/src/github.com/TristanCacqueray/dot-files/rcrc) ~/.rcrc
+rcup -v -d ~/src/github.com/TristanCacqueray/dot-files
+~/src/github.com/TristanCacqueray/dot-files/init.sh
+EOF
+
+function log {
+    echo -e "\x1b[01;01;32m[+] $1\x1b[00m"
+}
+
+function podenv {
+    log "Setup podenv"
+    mkdir -p ~/src/github.com/podenv/
+    git clone https://github.com/podenv/podenv ~/src/github.com/podenv/podenv
+    git clone https://github.com/podenv/hub ~/src/github.com/podenv/hub
+    git clone https://github.com/podenv/devenv ~/src/github.com/podenv/devenv
+    python3 -mpip install --user ~/src/github.com/podenv/podenv
+}
+
+function dnsmasq {
+    log "Setup dummy conn"
+    cat << EOF | sudo tee /etc/NetworkManager/system-connections/dummy-local0.nmconnection > /dev/null
+[connection]
+id=dummy-local0
+uuid=$(uuidgen)
+type=dummy
+interface-name=local0
+
+[ipv4]
+address1=192.168.42.42/32
+dns-search=
+method=manual
+
+[ipv6]
+addr-gen-mode=stable-privacy
+dns-search=
+method=auto
+EOF
+    sudo chmod 0600 /etc/NetworkManager/system-connections/dummy-local0.nmconnection
+
+    log "Enable dnsmasq"
+    cat << EOF | sudo tee /etc/NetworkManager/conf.d/00-use-dnsmasq.conf > /dev/null
+[main]
+dns=dnsmasq
+EOF
+    sudo chmod 0600 /etc/NetworkManager/conf.d/00-use-dnsmasq.conf
+
+    log "enable /etc/hosts resolv"
+    cat << EOF | sudo tee /etc/NetworkManager/dnsmasq.d/02-add-hosts.conf > /dev/null
+listen-address=127.0.0.1,192.168.42.42
+addn-hosts=/etc/hosts
+cache-size=4096
+resolv-file=/etc/resolv.conf
+EOF
+    sudo chmod 0600 /etc/NetworkManager/dnsmasq.d/02-add-hosts.conf
+}
+
+function gnome {
+    log "Setup GNOME workspace"
+    gsettings set org.gnome.mutter dynamic-workspaces false
+    gsettings set org.gnome.desktop.wm.preferences num-workspaces 9
+    for i in $(seq 1 9); do gsettings set org.gnome.shell.keybindings switch-to-application-${i} []; done
+    for i in $(seq 1 9); do gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-${i} "['<Super><Shift>${i}']"; done
+    for i in $(seq 1 9); do gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-${i} "['<Super>${i}']"; done
+
+    log "Disable sleep"
+    gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+}
+
+podenv
+dnsmasq
+gnome
